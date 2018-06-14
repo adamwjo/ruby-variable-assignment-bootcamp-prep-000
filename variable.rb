36341cd546ff742e14d7b90752556abbe19b3e0a
Adam Johnson <adamwjo@gmail.com>
2018-06-14 22:20:39 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,3 @@
  # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-x = greeting
+ # give it the value of the string "Hello Wor
 greeting = "Hello World"
